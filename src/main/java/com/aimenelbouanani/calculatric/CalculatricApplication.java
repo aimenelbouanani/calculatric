9f44533645d9d3d16518976c454bac46f2cd21dc
aimen <aimenelbouanani@gmail.com>
2024-10-22 17:22:50 +0100

    Ajouter le squellete spring boot

---
+++ src/main/java/com/aimenelbouanani/calculatric/CalculatricApplication.java
@@ -0,0 +1,13 @@
+package com.aimenelbouanani.calculatric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatricApplication.class, args);
+	}
+
+}

4e61a13ee6c48588867f375f7c7f4e8c68c46604
aimenelbouanani <aimenelbouanani@gmail.com>
2024-11-08 23:18:13 +0000

    commit

--- src/main/java/com/aimenelbouanani/calculatric/CalculatricApplication.java
+++ src/main/java/com/aimenelbouanani/calculatric/CalculatricApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatricApplication {
+    private static final String constant = "constant";
     public static void main(String[] args) {
         SpringApplication.run(CalculatricApplication.class, args);
     }
